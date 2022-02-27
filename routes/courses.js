2550214da5030ec70bf9cc3d6ca15fa4f893087f
tran0460 <tran0460@algonquinlive.com>
2022-02-26 19:26:50 -0500

    added OUT route and error funtion for courses

--- routes/courses.js
+++ routes/courses.js
@@ -46,6 +46,28 @@ router.patch('/:id', async (req, res) => {
         sendResourceNotFound(req, res)
     }
 })
+router.put('/:id', async (req, res) => {
+    try {
+        const {_id, ...attributes} = req.body.data.attributes
+        const course = await Course.findByIdAndUpdate(
+            req.params.id, 
+            {_id: req.params.id, ...attributes},
+            {
+                new: true,
+                overwrite: true,
+                runValidators: true
+            }
+        )
+        if(!course) {
+            throw new Error('Resource not found')
+        }
+        res.json({data: formatResponseData('courses', course.toObject())})
+    res.json({data: formatResponseData('courses', course.toObject())})
+    }
+    catch (error) {
+        sendResourceNotFound(req, res)
+    }
+})
 function formatResponseData(type, resource) {
     const {_id, ...attributes} = resource
     return {type, id: _id, attributes}
