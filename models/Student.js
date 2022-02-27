16c3fa291cb2686447c4918f0cdc63d0a8a17d41
tran0460 <tran0460@algonquinlive.com>
2022-02-26 19:02:06 -0500

    Added Course Model

--- models/Student.js
+++ models/Student.js
@@ -1,13 +1,9 @@
-const Student = require("./Student");
 const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
-    code: { type: String, require: true, maxlength: 16 },
-    title: { type: String, require: true, maxlength: 255 },
-    description: { type: String, require: false, maxlength: 2048 },
-    url: { type: String, require: false, maxlength: 512 },
-    students: [
-    { type: mongoose.Schema.Types.ObjectId, require: false, ref: "Student" },
-    ],
+    firstName: { type: String, require: true, maxlength: 64 },
+    lastName: { type: String, require: true, maxlength: 64 },
+    nickName: { type: String, require: true, maxlength: 64 },
+    eMail: { type: String, require: true, maxlength: 512 },
 });
-const Model = mongoose.model("Course", schema);
+const Model = mongoose.model("Student", schema);
 module.exports = Model;
