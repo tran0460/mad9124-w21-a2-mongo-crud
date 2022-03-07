959cca57941bb7ecf18561e06bbda72b05b64201
tran0460 <tran0460@algonquinlive.com>
2022-03-07 18:09:51 -0500

    removed sanitizeMongo in app.use

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(sanitizeMongo())
 
-app.use("/api/courses",  sanitizeMongo() , require("./routes/courses"));
-app.use("/api/students",  sanitizeMongo() , require("./routes/students"));
+app.use("/api/courses" , require("./routes/courses"));
+app.use("/api/students" , require("./routes/students"));
 
 const port = process.env.PORT || 3030;
 app.listen(port, () => console.log(`HTTP server listening on port ${port}...`));
