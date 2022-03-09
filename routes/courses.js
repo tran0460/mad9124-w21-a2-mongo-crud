0459970eb3d7bee3bb28b38551d9d46f4e0e3ec4
tran0460 <tran0460@algonquinlive.com>
2022-03-08 21:38:33 -0500

    fixed sanitizing in patch and put for both

--- routes/courses.js
+++ routes/courses.js
@@ -12,8 +12,20 @@ router.post('/', sanitizeBody, async (req, res) => {
     let attributes = req.body.data.attributes 
     delete attributes._id 
     let newCourse = new Course(attributes)
-    await newCourse.save()
-    res.status(201).json({data: formatResponseData('courses', newCourse.toObject())})
+    try {
+        await newCourse.save()
+        res.status(201).json({data: formatResponseData('courses', newCourse.toObject())})
+    } catch (err) {
+        res.status(500).send({
+            errors: [
+                {
+                status: '500',
+                title: 'Server error',
+                description: 'Problem saving document to the database.',
+                },
+            ],
+        })
+    }
 })
 router.get('/:id', async (req, res) => {
     try {
@@ -28,11 +40,11 @@ router.get('/:id', async (req, res) => {
     }
 })
 router.patch('/:id', sanitizeBody,  async (req, res) => {
+    const { _id, ...attributes } = req.body.data.attributes
     try {
-        const {_id, ...attributes} = req.body.data.attributes
         const course = await Course.findByIdAndUpdate(
             req.params.id, 
-            {_id: req.params.id, ...attributes},
+            { _id: req.params.id, ...attributes},
             {
                 new: true,
                 runValidators: true
@@ -42,15 +54,14 @@ router.patch('/:id', sanitizeBody,  async (req, res) => {
             throw new Error('Resource not found')
         }
         res.json({data: formatResponseData('courses', course.toObject())})
-    res.json({data: formatResponseData('courses', course.toObject())})
     }
     catch (error) {
         sendResourceNotFound(req, res)
     }
 })
 router.put('/:id', sanitizeBody, async (req, res) => {
+    const {_id, ...attributes} = req.body.data.attributes
     try {
-        const {_id, ...attributes} = req.body.data.attributes
         const course = await Course.findByIdAndUpdate(
             req.params.id, 
             {_id: req.params.id, ...attributes},
@@ -60,11 +71,11 @@ router.put('/:id', sanitizeBody, async (req, res) => {
                 runValidators: true
             }
         )
+        console.log(course)
         if(!course) {
             throw new Error('Resource not found')
         }
         res.json({data: formatResponseData('courses', course.toObject())})
-    res.json({data: formatResponseData('courses', course.toObject())})
     }
     catch (error) {
         sendResourceNotFound(req, res)
--- routes/students.js
+++ routes/students.js
@@ -12,8 +12,20 @@ router.post('/', sanitizeBody, async (req, res) => {
     let attributes = req.body.data.attributes 
     delete attributes._id 
     let newStudent = new Student(attributes)
-    await newStudent.save()
-    res.status(201).json({data: formatResponseData('people', newStudent.toObject())})
+    try {
+        await newStudent.save()
+        res.status(201).json({data: formatResponseData('people', newStudent.toObject())})
+    } catch (err) {
+        res.status(500).send({
+            errors: [
+                {
+                status: '500',
+                title: 'Server error',
+                description: 'Problem saving document to the database.',
+                },
+            ],
+        })
+    }
 })
 router.get('/:id', async (req, res) => {
     try {
@@ -28,8 +40,8 @@ router.get('/:id', async (req, res) => {
     }
 }) 
 router.patch('/:id', sanitizeBody, async (req, res) => {
+    const {_id, ...attributes} = req.body.data.attributes
     try {
-        const {_id, ...attributes} = req.body.data.attributes
         const student = await Student.findByIdAndUpdate(
             req.params.id, 
             {_id: req.params.id, ...attributes},
@@ -42,15 +54,14 @@ router.patch('/:id', sanitizeBody, async (req, res) => {
             throw new Error('Resource not found')
         }
         res.json({data: formatResponseData('people', student.toObject())})
-    res.json({data: formatResponseData('people', student.toObject())})
     }
     catch (error) {
         sendResourceNotFound(req, res)
     }
 })
 router.put('/:id', sanitizeBody, async (req, res) => {
+    const {_id, ...attributes} = req.body.data.attributes
     try {
-        const {_id, ...attributes} = req.body.data.attributes
         const student = await Student.findByIdAndUpdate(
             req.params.id, 
             {_id: req.params.id, ...attributes},
@@ -64,7 +75,6 @@ router.put('/:id', sanitizeBody, async (req, res) => {
             throw new Error('Resource not found')
         }
         res.json({data: formatResponseData('people', student.toObject())})
-    res.json({data: formatResponseData('people', student.toObject())})
 }
     catch (error) {
         sendResourceNotFound(req, res)
