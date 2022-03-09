741b273b2b5770c28f9a1f5976b5e0efc1c91796
tran0460 <tran0460@algonquinlive.com>
2022-03-08 21:48:11 -0500

    made nickname not required

--- models/Student.js
+++ models/Student.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const schema = new mongoose.Schema({
     firstName: { type: String, required: true, maxlength: 64 },
     lastName: { type: String, required: true, maxlength: 64 },
-    nickName: { type: String, required: true, maxlength: 64 },
-    eMail: { type: String, required: true, maxlength: 512 },
+    nickName: { type: String, required: false, maxlength: 64 },
+    email: { type: String, required: true, maxlength: 512 },
 });
 const Model = mongoose.model("Student", schema);
 module.exports = Model;
